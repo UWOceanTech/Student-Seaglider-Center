e42f68ea5427d85c8aba370270aa485d2f8291ea
bellaandre <aandre22@uw.edu>
2025-09-04 17:07:17 -0700

    adding files

---
+++ 2024_10_11_shilshole_shakedown_2/misc data/flight/fm_0001.m
@@ -0,0 +1,12 @@
+% Dive 1 as of 11 Oct 2024 20:37:39
+% SG195 Shileshole Shakedown 10Oct2024
+volmax = 53410.7;
+vbdbias = 234.314; % vbdbias w rms = 0.41 cm/s
+volmax_biased = 53176.4;
+abs_compress = 4.5262e-06;
+hd_a = 0.00223872;
+hd_b = 0.0163117;
+hd_c = 5.7e-06;
+hd_s = -0.25;
+therm_expan = 7.05e-05;
+temp_ref = 15;
