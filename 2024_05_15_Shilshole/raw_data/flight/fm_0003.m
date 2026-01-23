1fc9a93222e04a04fd89cc4bc7768f5ee8e3b06c
UW Ocean Technology Program <136491027+UWOceanTech@users.noreply.github.com>
2026-01-22 22:11:02 -0800

    c

---
+++ 2024_05_15_Shilshole/raw_data/flight/fm_0003.m
@@ -0,0 +1,12 @@
+% Dive 3 as of 15 May 2024 19:52:07
+% SG195 Shilshole Overnight OCN 201, 15May2024
+volmax = 53235.7;
+vbdbias = 160.608; % vbdbias w rms = 0.87 cm/s
+volmax_biased = 53075.1;
+abs_compress = 8.63627e-07;
+hd_a = 0.00199526;
+hd_b = 0.0191095;
+hd_c = 5.7e-06;
+hd_s = -0.25;
+therm_expan = 7.05e-05;
+temp_ref = 15;
