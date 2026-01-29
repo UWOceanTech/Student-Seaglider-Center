ea19925ad7ce1bc3f38299b0ba2b0e47f7b2b843
lydiakelley <135685647+lydiakelley@users.noreply.github.com>
2026-01-29 12:36:52 -0800

    c

---
+++ 2024_05_15_Shilshole/raw_files/flight/fm_0001.m
@@ -0,0 +1,12 @@
+% Dive 1 as of 15 May 2024 19:51:54
+% SG195 Shilshole Overnight OCN 201, 15May2024
+volmax = 53235.7;
+vbdbias = 107.805; % vbdbias w rms = 0.54 cm/s
+volmax_biased = 53127.9;
+abs_compress = 8.13637e-06;
+hd_a = 0.00199526;
+hd_b = 0.0191095;
+hd_c = 5.7e-06;
+hd_s = -0.25;
+therm_expan = 7.05e-05;
+temp_ref = 15;
