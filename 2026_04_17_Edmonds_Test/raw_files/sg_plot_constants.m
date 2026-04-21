81093de5c29e2076ec9a1621987db68435147f0d
lydiakelley <135685647+lydiakelley@users.noreply.github.com>
2026-04-21 20:12:27 +0000

    c

---
+++ 2026_04_17_Edmonds_Test/raw_files/sg_plot_constants.m
@@ -0,0 +1,11 @@
+lat_north = 47.90;
+lat_south = 47.65;
+lon_west = -122.5;
+lon_east = -122.3;
+
+vector_start = 3;
+vector_end = 10000;
+
+section_start = 3;
+section_max_z = 200;
+section_dz = 5;
