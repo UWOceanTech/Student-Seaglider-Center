ae8e4cf1e5d88ec97d81b62de121d6b5ffa76817
Sophie Monterola <somont@uw.edu>
2026-05-20 07:28:54 -0700

    c

---
+++ 2026_05_18_Edmonds_Test/raw_files/st0064du.r
@@ -0,0 +1,22 @@
+version: 66.11
+glider: 174
+mission: 13
+selftest: 64
+start: 3 2 126 21 17 46
+columns: rec,elaps_tms,depth,heading,pitch,roll,AD_pitch,AD_roll,AD_vbd,GC_phase,mag.x,mag.y,mag.z,sbect.TempFreq,sbect.CondFreq,sbe43.O2Freq,aa4330.O2,aa4330.AirSat,aa4330.Temp,aa4330.CalPhase,aa4330.TCPhase,wlbb2f.redCount,wlbb2f.blueCount,wlbb2f.fluorCount,wlbb2f.VFtemp,
+data:
+0 5270 3 2107 -39 -9469 763 2184 500 6 -969 -26 -845 211760 345291 T T T T T T 51 57 53 540
+1 5296 -6 19 -1 -46 0 0 0 0 0 -1 0 -399 1 T T T T T T 0 0 0 0
+1 5564 0 -23 0 53 0 0 0 0 -1 1 2 -1089 -1 T T T T T T 0 -6 0 0
+1 5323 7 12 0 -27 0 0 0 0 4 2 -2 -1157 3 T T T T T T 0 4 0 0
+1 5321 -4 -9 0 20 0 0 0 0 -2 -2 1 912 0 T T T T T T 0 3 0 0
+1 5552 -3 3 0 -6 0 0 0 0 -1 0 0 -119 0 T T T T T T -1 5 0 -1
+1 5937 3 -15 0 33 0 0 0 0 0 0 1 287 0 T T T T T T 1 -7 0 0
+1 5935 -3 18 3 -40 0 0 0 0 0 0 -1 27 0 T T T T T T 0 -1 0 0
+1 5941 5 10 -6 -26 0 0 0 0 0 0 0 1148 -1 T T T T T T -1 0 0 0
+1 6192 -4 -7 3 19 0 0 0 0 -1 0 -1 131 0 T T T T T T 0 -1 0 0
+1 5964 -3 -3 -3 7 0 0 0 0 1 0 0 -588 -1 T T T T T T 1 4 0 0
+1 5334 5 -36 3 79 0 0 0 0 -1 0 -1 -675 2 T T T T T T -1 1 0 0
+1 5588 -5 33 1 -72 0 0 0 0 1 1 1 -264 -1 T T T T T T 1 -1 0 0
+1 5346 11 11 -2 -27 0 0 0 0 -1 -1 0 -1596 1 T T T T T T 0 -1 0 0
+1 5336 -10 -24 3 60 0 0 0 0 4 2 -1 -697 0 T T T T T T 0 1 0 0
---
+++ 2026_05_18_Edmonds_Test/raw_files/st0067du.r
@@ -0,0 +1,22 @@
+version: 66.11
+glider: 174
+mission: 13
+selftest: 67
+start: 5 15 126 21 24 43
+columns: rec,elaps_tms,depth,heading,pitch,roll,AD_pitch,AD_roll,AD_vbd,GC_phase,mag.x,mag.y,mag.z,sbect.TempFreq,sbect.CondFreq,sbe43.O2Freq,aa4330.O2,aa4330.AirSat,aa4330.Temp,aa4330.CalPhase,aa4330.TCPhase,wlbb2f.redCount,wlbb2f.blueCount,wlbb2f.fluorCount,wlbb2f.VFtemp,
+data:
+0 5270 -9 3470 -579 -7557 758 2194 541 6 528 243 -216 229507 345294 T 319400 97570 13239 31320 31320 51 60 52 562
+1 4620 10 10 -2 -13 0 0 0 0 -1 -4 0 -303 -2 T T T T T T 0 -2 1 -2
+1 5583 4 10 2 -18 0 0 0 0 0 2 0 -32 4 T T T T T T 0 -1 0 0
+1 5317 -10 -23 0 37 0 0 0 0 0 1 0 126 -3 T T T T T T 0 3 0 0
+1 5329 10 0 2 0 0 0 0 0 -1 -1 1 20 0 T T T T T T 0 -1 0 0
+1 5556 -2 -12 -3 19 0 0 0 0 0 1 1 447 -1 T T T T T T 0 -2 0 0
+1 5939 -3 15 3 -25 0 0 0 0 1 1 -1 380 -2 T T T T T T 0 3 0 0
+1 5939 0 -50 -2 86 0 0 0 0 -1 1 -1 678 4 T T T T T T 0 -2 0 -1
+1 5936 1 21 0 -37 0 0 0 0 -1 -1 0 75 -1 T T T T T T 0 0 0 1
+1 6194 0 25 -2 -43 0 0 0 0 1 -1 1 -436 1 T T T T T T 0 -1 0 -1
+1 5959 -5 -20 -2 37 0 0 0 0 0 0 -2 -162 0 T T T T T T 0 -2 0 0
+1 5339 3 40 6 -68 0 0 0 0 1 0 0 58 1 T T T T T T 0 2 0 0
+1 5586 9 -36 -1 62 0 0 0 0 -1 1 0 -334 -1 T T T T T T 0 -2 0 0
+1 5343 -5 3 -3 0 0 0 0 0 0 -4 -1 -379 -2 T T T T T T 0 3 0 0
+1 5343 -2 12 0 -25 0 0 0 0 1 1 2 -45 3 T T T T T T 0 -2 0 0
