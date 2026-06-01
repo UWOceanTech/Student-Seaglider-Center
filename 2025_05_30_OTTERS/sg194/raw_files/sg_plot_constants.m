283a5b8d0b11f5bf83f8701dfcac25342f833933
lydiakelley <135685647+lydiakelley@users.noreply.github.com>
2026-06-01 05:26:51 +0000

    c

---
+++ 2025_05_30_OTTERS/sg194/raw_files/sg_plot_constants.m
@@ -0,0 +1,11 @@
+lat_north = 47.72;
+lat_south = 47.55;
+lon_west = -123.0;
+lon_east = -122.85;
+
+vector_start = 3;
+vector_end = 10000;
+
+section_start = 3;
+section_max_z = 200;
+section_dz = 5;
