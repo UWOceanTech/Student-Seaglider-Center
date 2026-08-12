a9dfce3a8acd5c35487858d954fe6e44bb53bfe6
jlinpf07 <jlinpf@uw.edu>
2026-08-12 15:55:36 -0700

    Add files via upload

---
+++ 2026_07_17_Shilshole_Test/raw_files/sg_plot_constants.m
@@ -0,0 +1,11 @@
+lat_north = 47.90;
+lat_south = 47.60;
+lon_west = -122.37;
+lon_east = -122.51;
+
+vector_start = 3;
+vector_end = 10000;
+
+section_start = 3;
+section_max_z = 200;
+section_dz = 5;
